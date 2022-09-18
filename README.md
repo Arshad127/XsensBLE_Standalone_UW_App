7605fbafd5d02e4a03a0aa71cc64f66b5da25ef3
Arshad127 <64776294+Arshad127@users.noreply.github.com>
2022-09-18 13:20:43 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# XsensBLE_Standalone_UW_App
+ Xsens DOT Communication Application created using the Universal Windows Platform
---
+++ XsensBLE_Communication/MainPage.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using SDKTemplate;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace XsensBLE_Communication
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<BluetoothLEDeviceDisplay> KnownDevices = new ObservableCollection<BluetoothLEDeviceDisplay>();
+        private List<DeviceInformation> UnknownDevices = new List<DeviceInformation>();
+
+        private DeviceWatcher deviceWatcher;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MessageBox.Text = "[INFO] Message Box";
+            StreamBox.Text = "Streaming Result Box";
+        }
+
+        private void DiscoveringButton_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            if (deviceWatcher == null)
+            {
+                StartBleDeviceWatcher();
+                DiscoveringButton.Content = "Stop discovering";
+                rootPage.NotifyUser($"Device watcher started.", NotifyType.StatusMessage);
+            }
+            else
+            {
+                StopBleDeviceWatcher();
+                EnumerateButton.Content = "Start enumerating";
+                rootPage.NotifyUser($"Device watcher stopped.", NotifyType.StatusMessage);
+            }
+            */
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ResetHeadingButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SyncButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StreamingButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #region Device Discovery Methods
+
+        /// <summary>
+        /// Starts a device watcher that looks for all nearby Bluetooth devices (paired or unpaired). 
+        /// Attaches event handlers to populate the device collection.
+        /// </summary>
+        private void StartBleDeviceWatcher()
+        {
+            // Additional properties we would like about the device.
+            // Property strings are documented here https://msdn.microsoft.com/en-us/library/windows/desktop/ff521659(v=vs.85).aspx
+            string[] requestedProperties = { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected", "System.Devices.Aep.Bluetooth.Le.IsConnectable" };
+
+            // BT_Code: Example showing paired and non-paired in a single query.
+            string aqsAllBluetoothLEDevices = "(System.Devices.Aep.ProtocolId:=\"{bb7bb05e-5972-42b5-94fc-76eaa7084d49}\")";
+
+            deviceWatcher =
+                    DeviceInformation.CreateWatcher(
+                        aqsAllBluetoothLEDevices,
+                        requestedProperties,
+                        DeviceInformationKind.AssociationEndpoint);
+
+            // Register event handlers before starting the watcher.
+            deviceWatcher.Added += DeviceWatcher_Added;
+            deviceWatcher.Updated += DeviceWatcher_Updated;
+            deviceWatcher.Removed += DeviceWatcher_Removed;
+            deviceWatcher.EnumerationCompleted += DeviceWatcher_EnumerationCompleted;
+            deviceWatcher.Stopped += DeviceWatcher_Stopped;
+
+            // Start over with an empty collection.
+            KnownDevices.Clear();
+
+            // Start the watcher. Active enumeration is limited to approximately 30 seconds.
+            // This limits power usage and reduces interference with other Bluetooth activities.
+            // To monitor for the presence of Bluetooth LE devices for an extended period,
+            // use the BluetoothLEAdvertisementWatcher runtime class. See the BluetoothAdvertisement
+            // sample for an example.
+            deviceWatcher.Start();
+        }
+
+        /// <summary>
+        /// Stops watching for all nearby Bluetooth devices.
+        /// </summary>
+        private void StopBleDeviceWatcher()
+        {
+            if (deviceWatcher != null)
+            {
+                // Unregister the event handlers.
+                deviceWatcher.Added -= DeviceWatcher_Added;
+                deviceWatcher.Updated -= DeviceWatcher_Updated;
+                deviceWatcher.Removed -= DeviceWatcher_Removed;
+                deviceWatcher.EnumerationCompleted -= DeviceWatcher_EnumerationCompleted;
+                deviceWatcher.Stopped -= DeviceWatcher_Stopped;
+
+                // Stop the watcher.
+                deviceWatcher.Stop();
+                deviceWatcher = null;
+            }
+        }
+
+        private BluetoothLEDeviceDisplay FindBluetoothLEDeviceDisplay(string id)
+        {
+            foreach (BluetoothLEDeviceDisplay bleDeviceDisplay in KnownDevices)
+            {
+                if (bleDeviceDisplay.Id == id)
+                {
+                    return bleDeviceDisplay;
+                }
+            }
+            return null;
+        }
+
+        private DeviceInformation FindUnknownDevices(string id)
+        {
+            foreach (DeviceInformation bleDeviceInfo in UnknownDevices)
+            {
+                if (bleDeviceInfo.Id == id)
+                {
+                    return bleDeviceInfo;
+                }
+            }
+            return null;
+        }
+
+        private async void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation deviceInfo)
+        {
+
+        }
+
+        private async void DeviceWatcher_Updated(DeviceWatcher sender, DeviceInformationUpdate deviceInfoUpdate)
+        {
+
+        }
+
+        private async void DeviceWatcher_Removed(DeviceWatcher sender, DeviceInformationUpdate deviceInfoUpdate)
+        {
+
+        }
+
+        private async void DeviceWatcher_EnumerationCompleted(DeviceWatcher sender, object e)
+        {
+
+        }
+
+        private async void DeviceWatcher_Stopped(DeviceWatcher sender, object e)
+        {
+
+        }
+
+        #endregion
+    }
+}
